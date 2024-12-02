8a9d3bb8dd353bca2666ed4b9857a793be64681d
mollysandler <74861421+mollysandler@users.noreply.github.com>
2024-12-02 14:34:00 -0800

    Style updates

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import ProfileDropdown from "./ProfileDropdown"; // Import ProfileDropdown
 import "./styles/navBar.css";
+import image from "../assets/small.jpeg";
 
 const SportButtons = () => {
   return (
     <nav className="navbar">
+      {/* <div className="navbar-logo">
+        <Link to="/about">
+          <img src={image} alt="Logo" className="logo-image" />
+        </Link>
+      </div> */}
       <ProfileDropdown /> {/* Include ProfileDropdown here */}
       <ul className="navbar-menu">
         <li className="navbar-item">
